da2a40f09f4f3010153412bdb0a33f2d4ba911bd
hemanth.chinthalapudi <hemanth.chinthalapudi@kony.com>
2015-12-21 12:12:21 +0530

    Corrected problem description.

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -1,5 +1,6 @@
 /*
-OVERVIEW: You are given 2 bank statements that are ordered by date - Write a function that produces a single merged statement ordered by dates.
+OVERVIEW: You are given a bank statement where transactions are ordered by date. Write a function that finds the number of transactions in that statement after a given date.
+-- find the count of numbers greater than a given number in a sorted array.
 E.g.: Input: A[3] = { { 10, "09-10-2003", "First" }, { 20, "19-10-2004", "Second" }, { 30, "03-03-2005", "Third" } };
 date = "19-10-2004"
 Output: 1 (only one transaction : { 30, "03-03-2005", "Third" })
