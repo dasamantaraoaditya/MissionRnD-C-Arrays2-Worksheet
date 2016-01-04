1d731ca0155609c97c28f33519716b4ea7bb2d20
Hemanth Reddy <hemanthreddy519@gmail.com>
2016-01-04 14:49:12 +0530

    Updated problem description.

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -3,7 +3,7 @@ OVERVIEW: You are given a bank statement where transactions are ordered by date.
 -- find the count of numbers greater than a given number in a sorted array.
 E.g.: Input: A[3] = { { 10, "09-10-2003", "First" }, { 20, "19-10-2004", "Second" }, { 30, "03-03-2005", "Third" } };
 date = "19-10-2004"
-Output: 1 (only one transaction : { 30, "03-03-2005", "Third" })
+Output: 1 (as there is only one transaction { 30, "03-03-2005", "Third" })
 
 INPUTS: One bank statement (array of transactions) and date.
 
